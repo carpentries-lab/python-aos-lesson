f7c19d68ddc3469cf3e267c73a0e5386b1b817f9
Damien Irving <irving.damien@gmail.com>
2018-08-27 08:27:20 -0400

    Switched to xarray

--- code/plot_precipitation_climatology.py
+++ code/plot_precipitation_climatology.py
@@ -1,67 +1,70 @@
 import argparse
-import iris
+import xarray
 import matplotlib.pyplot as plt
-import iris.plot as iplt
-import iris.coord_categorisation
-import cmocean
+import cartopy.crs as ccrs
 import numpy
+import cmocean
 
 
-def read_data(fname, month):
-    """Read an input data file"""
+def convert_pr_units(darray):
+    """Convert kg m-2 s-1 to mm day-1.
     
-    cube = iris.load_cube(fname, 'precipitation_flux')
+    Args:
+      darray (xarray.core.dataarray.DataArray): Precipitation data
     
-    iris.coord_categorisation.add_month(cube, 'time')
-    cube = cube.extract(iris.Constraint(month=month))
+    """
     
-    return cube
+    darray.data = darray.data * 86400
+    darray.attrs['units'] = 'mm/day'
+    
+    return darray
 
 
-def convert_pr_units(cube):
-    """Convert kg m-2 s-1 to mm day-1"""
+def plot_climatology(clim_darray, model_name, season, gridlines=False):
+    """Plot the precipitation climatology.
     
-    cube.data = cube.data * 86400
-    cube.units = 'mm/day'
+    Args:
+      clim_darray (xarray.core.dataarray.DataArray): Precipitation climatology data
+      season (str): Season    
     
-    return cube
-
-
-def plot_data(cube, month, gridlines=False):
-    """Plot the data."""
+    """
         
-    fig = plt.figure(figsize=[12,5])    
-    iplt.contourf(cube, cmap=cmocean.cm.haline_r, 
-                  levels=numpy.arange(0, 10),
-                  extend='max')
-
-    plt.gca().coastlines()
+    fig = plt.figure(figsize=[12,5])
+    ax = fig.add_subplot(111, projection=ccrs.PlateCarree(central_longitude=180))
+    clim_darray.sel(season=season).plot.contourf(ax=ax,
+                                                 levels=numpy.arange(0, 15, 1.5),
+                                                 extend='max',
+                                                 transform=ccrs.PlateCarree(),
+                                                 cbar_kwargs={'label': clim_darray.units},
+                                                 cmap=cmocean.cm.haline_r)
+    ax.coastlines()
     if gridlines:
         plt.gca().gridlines()
-    cbar = plt.colorbar()
-    cbar.set_label(str(cube.units))
     
-    title = '%s precipitation climatology (%s)' %(cube.attributes['model_id'], month)
+    title = '%s precipitation climatology (%s)' %(model_name, season)
     plt.title(title)
 
 
 def main(inargs):
     """Run the program."""
 
-    cube = read_data(inargs.infile, inargs.month)    
-    cube = convert_pr_units(cube)
-    clim = cube.collapsed('time', iris.analysis.MEAN)
-    plot_data(clim, inargs.month)
-    plt.savefig(inargs.outfile)
+    dset = xarray.open_dataset(inargs.pr_file)
+    pr_darray = dset['pr']
+    
+    clim_darray = pr_darray.groupby('time.season').mean(dim='time')
+    clim_darray = convert_pr_units(clim_darray)
+
+    plot_climatology(clim_darray, dset.attrs['model_id'], inargs.season)
+    plt.savefig(inargs.output_file, dpi=200)
 
 
 if __name__ == '__main__':
     description='Plot the precipitation climatology.'
     parser = argparse.ArgumentParser(description=description)
     
-    parser.add_argument("infile", type=str, help="Input file name")
-    parser.add_argument("month", type=str, help="Month to plot")
-    parser.add_argument("outfile", type=str, help="Output file name")
+    parser.add_argument("pr_file", type=str, help="Precipitation data file")
+    parser.add_argument("season", type=str, help="Season to plot")
+    parser.add_argument("output_file", type=str, help="Output file name")
 
     args = parser.parse_args()
     
